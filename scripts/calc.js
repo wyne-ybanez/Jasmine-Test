071037a8390f4a7c3b23921d3b3a5b20820ad414
wyne-ybanez <wyne_01@icloud.com>
2020-12-10 22:19:12 +0000

    Add defenisive programming by using typeof method, check for numbers, return error if not numbers

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,8 @@
 function addition(numOne, numTwo) {
-    return numOne + numTwo;
+    if (typeof(numOne)== "number" && typeof(numTwo) == "number") {
+        return numOne + numTwo
+    }
+    else {
+        return "Error!"
+    }
 }
