85d20a0daef14647b2322063ae7b82f92f03606d
wyne-ybanez <wyne_01@icloud.com>
2020-12-11 13:28:13 +0000

    Add instance calc equal as New Calculator. 1 Error present, calc numbers adding together.

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,8 +1,13 @@
-function addition(numOne, numTwo) {
-    if (typeof(numOne)== "number" && typeof(numTwo) == "number") {
-        return numOne + numTwo
-    }
-    else {
+Calculator = function() {
+    this.value = 0
+}
+
+Calculator.prototype.add = function(number) {
+    if (typeof(number) == "number") {
+        this.value += number
+    } else {
         alert("Error!")
     }
 }
+
+
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,18 +1,24 @@
 describe("Calculator", function() {
-    describe("Addition tests", function() {
-        it ("should return 42", function () {
-            expect(addition(20,22)).toBe(42)
-        })
-        it ("should return 26", function () {
-            expect(addition(7, 19)).toBe(26)
-        })
-        it ("should return an error if we don't supply two numbers", function() {
-            // takes the window argument because Alert() is a window method
-            spyOn(window, "alert")
-            addition("Hitchhikers", "Guide")
-            // checks if "Error!" was used for calling the function
-            expect(window.alert).toHaveBeenCalledWith("Error!")
 
-        })
-    })
-})
+    var calc = new Calculator;
+
+    describe("Addition function", function() {
+        it("should return 42", function() {
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
+        });
+
+        it("should return 26", function() {
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
+        });
+
+        it("should return an error if we don't supply two numbers", function() {
+            spyOn(window, "alert");
+            calc.add("Hitchhikers");
+            expect(window.alert).toHaveBeenCalledWith("Error!");
+        });
+    });
+});
